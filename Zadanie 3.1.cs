1756745c26f06e95889d1ddad1c08512f244a78c
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 10:14:28 +0100

    Add files via upload

---
+++ Zadanie 3.1.cs
@@ -0,0 +1,21 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj Rok: ");
+            double rok = double.Parse(Console.ReadLine());
+
+            if ((rok % 4 == 0 && rok % 100 != 0) || rok%400 == 0)
+            {
+                Console.WriteLine("Rok jest przestępny");
+            }
+            else
+            {
+                Console.WriteLine("Rok nie jest przestepny");
+            }
+            Console.ReadKey();
+        }
+    }
+}
