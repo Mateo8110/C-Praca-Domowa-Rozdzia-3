a844bb8cc408b1e34c387d87b5e7a9b6a14d72aa
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:14:06 +0100

    Add files via upload

---
+++ Zadanie 3.6.cs
@@ -0,0 +1,32 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            double BMI = 0;
+            Console.WriteLine("Podaj swoją wagę w Kg: ");
+            double masa = double.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj wzrost w m: ");
+            double wzrost = double.Parse(Console.ReadLine());
+
+            BMI = masa / (wzrost * wzrost);
+
+            Console.WriteLine("Twoje BMI wynosi: {0}", BMI);
+
+            if (BMI < 18.5)
+            {
+                Console.WriteLine("Masz niedowagę");
+            }
+            else if ((BMI > 18.5) && (BMI < 24.99))
+            {
+                Console.WriteLine("Twoja waga jest prawidłowa");
+            }
+            else
+            {
+                Console.WriteLine("Masz nadwagę");
+            }
+            Console.ReadKey();
+        }
+    }
+}
