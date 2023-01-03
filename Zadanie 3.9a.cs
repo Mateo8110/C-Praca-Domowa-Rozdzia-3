25a208c3954e1847e23b2f730942a5ae241526f3
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:16:05 +0100

    Add files via upload

---
+++ Zadanie 3.9a.cs
@@ -0,0 +1,20 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadz liczbę wierszy");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
