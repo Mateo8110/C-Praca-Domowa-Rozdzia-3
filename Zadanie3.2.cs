7a424c1f64a50ae2b0b0d50311ed7119744764c8
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 10:15:40 +0100

    Add files via upload

---
+++ Zadanie3.2.cs
@@ -0,0 +1,25 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj 1 liczbę: ");
+            int liczba1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Podaj 2 liczbę: ");
+            int liczba2 = int.Parse(Console.ReadLine());
+
+            if (liczba1 % liczba2 == 0)
+            {
+                Console.WriteLine("2 liczba jest dzilenikiem liczby 1");
+            }
+            else
+            {
+                Console.WriteLine("2 liczba nie jest dzielnikiem liczby 1");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
+
