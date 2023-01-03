a5e2f7dde6f295a3a32f8da432b8597b161647f0
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:29:38 +0100

    Add files via upload

---
+++ Zadanie 3.14.cs
@@ -0,0 +1,27 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj przedział od <1,n>");
+            Console.WriteLine("Podaj n:");
+            
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                int suma = 0;
+                for (int j = 1; j < i; j++)
+                {
+                    if (i % j == 0)
+                        suma += j;
+                }
+                if (suma == i)
+                    Console.WriteLine("Liczba {0} jest liczbą doskonałą", i);
+            }
+            Console.ReadKey();
+  
+        }
+    }
+}
