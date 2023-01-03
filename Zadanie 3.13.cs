9fdec07662992fb74ebdbffd3dd6832129bd86a8
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:29:09 +0100

    Add files via upload

---
+++ Zadanie 3.13.cs
@@ -0,0 +1,26 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int n;
+            Console.WriteLine("Wczytaj n: ");
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    suma -= i;
+                }
+                else
+                {
+                    suma += i;
+                }
+            }
+            Console.WriteLine("Suma wynosi: {0}",suma);
+            Console.ReadKey();
+        }
+    }
+}
