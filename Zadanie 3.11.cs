0a03561325c3e9ce170151095c77e45e6f252ce9
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:28:05 +0100

    Add files via upload

---
+++ Zadanie 3.11.cs
@@ -0,0 +1,25 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            int ileLiczb = 1;
+            int suma = 0;
+            int poczatek = 0;
+
+            while (suma <= 100)
+            {
+                ileLiczb++;
+                suma = suma + poczatek;
+                poczatek++;
+                Console.WriteLine(suma);
+                
+
+            }
+            Console.WriteLine("Ilość potrzebnych liczb {0} a suma wynosi {1}", ileLiczb, suma);
+            Console.ReadKey();
+
+        }
+    }
+}
