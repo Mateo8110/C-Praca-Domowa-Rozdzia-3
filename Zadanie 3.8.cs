99d37d691648ec35e124b6cc505f1ea85cc1d17e
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:15:27 +0100

    Add files via upload

---
+++ Zadanie 3.8.cs
@@ -0,0 +1,26 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Jaka jest średnia twoich ocen?");
+            double srednia = double.Parse(Console.ReadLine());
+
+            if ((srednia > 2.00) && (srednia < 3.99))
+            {
+                Console.WriteLine("Kwota stypednium wynosi 0 zł");
+            }
+            else if ((srednia > 4.00) && (srednia < 4.79))
+            {
+                Console.WriteLine("Kwota stypendium wynosi 350 zł");
+            }
+            else
+            {
+                Console.WriteLine("Kwota stypendium wynosi 550 zł");
+            }
+            Console.ReadKey();
+        }
+    }
+}
