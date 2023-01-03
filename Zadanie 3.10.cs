9d131c19ed7dd0f1a74663401766709eae34ac0d
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:27:14 +0100

    Add files via upload

---
+++ Zadanie 3.10.cs
@@ -0,0 +1,21 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            int silnia = 1;
+            Console.WriteLine("Podaj n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i > 0; i--)
+            {
+                silnia = silnia * i;
+            }
+            Console.WriteLine("Silnia wynosi: {0}", silnia);
+            Console.ReadKey();
+        }
+    }
+}
+
+        
