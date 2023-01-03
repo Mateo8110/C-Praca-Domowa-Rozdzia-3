d01f6d007e05aad2b470efda47a4f787c757b55f
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:30:08 +0100

    Add files via upload

---
+++ Zadanie 3.15.cs
@@ -0,0 +1,24 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Na ile sposobów można wypłacić 10 złotych");
+            for (int k1 = 0; k1 <= 10; k1++)
+            {
+                for (int k2 = 0; k2 <= 10; k2++)
+                {
+                    for (int k3 = 0; k3 <= 10; k3++)
+                    {
+                        if (k1 * 1 + k2 * 2 + k3 * 5 == 10)
+                            Console.WriteLine("Złotówka = {0} Dwa Złote = {1} Pięć Złotych = {2}",
+                                k1, k2, k3);
+
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
