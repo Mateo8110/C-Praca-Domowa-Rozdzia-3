b1131c36d35eafbe9023956029c33eafcfda6a68
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:28:36 +0100

    Add files via upload

---
+++ Zadanie 3.12.cs
@@ -0,0 +1,30 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            int suma = 0;
+            int a;
+
+            do
+            {
+                Console.WriteLine("Podaj liczbę całkowitą: ");
+                a = int.Parse(Console.ReadLine());
+                suma += a;
+                Console.Clear();
+                
+            }
+            while (a != 0);
+            {
+                Console.WriteLine("Suma liczb wynosi {0}", suma);
+                Console.ReadLine();
+            }
+            Console.ReadKey();
+        }      
+    }
+}
+
+
+            
+            
