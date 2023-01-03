27f4344dff0da3083eb936977b1068b2cc445b1d
Mateo8110 <94134843+Mateo8110@users.noreply.github.com>
2023-01-03 12:14:41 +0100

    Add files via upload

---
+++ Zadanie 3.7.cs
@@ -0,0 +1,41 @@
+﻿namespace Program
+{
+    class Zadanie
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz nr dnia tygodnia: ");
+            string numer = Console.ReadLine();
+
+            switch (numer)
+            {
+                case "1":
+                    Console.WriteLine("Poniedziałek");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Wtorek");
+                    break;
+                case "3":
+                    Console.WriteLine("Środa");
+                    break;
+                case "4":
+                    Console.WriteLine("Czwartek");
+                    break;
+                case "5":
+                    Console.WriteLine("Piątek");
+                    break;
+                case "6":
+                    Console.WriteLine("Sobota");
+                    break;
+                case "7":
+                    Console.WriteLine("Niedziela");
+                    break;
+                default:
+                    Console.WriteLine("wartosc nieprawidłowa");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
